f15a4490ba24c3af759395393f0341a9a2d70e01
test <test@test.com>
2024-05-10 15:49:09 +0800

    add empty check_config

---
+++ main.py
@@ -0,0 +1,22 @@
+from typing import List
+
+def check_config(config: dict) -> bool:
+    """Check if errors exist in config variable
+
+    vim
+    Copy
+    Args:
+        config (dict): config dict variable to be check
+
+    Returns:
+        bool: true if no errors detected
+    """
+    res: bool = False
+
+    # checkpoint #1: if the binding argument list size equal
+    # add your code here
+
+    # checkpoint #2: template parameter integrity
+    # add your code here
+
+    return res
