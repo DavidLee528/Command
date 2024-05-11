8a654d5261e05f5569fa58443af13be14d65a5db
test <test@test.com>
2024-05-11 10:12:37 +0800

    test/execute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import command
 from typing import List
+from execution import execute
 
 if __name__ == "__main__":
     # passed: bool = command.check_config(config = command.config)
@@ -11,3 +12,5 @@ if __name__ == "__main__":
         with open("commands.txt", "w") as f:
             for command in command_list:
                 f.write(command + "\n")
+        
+        execute(command_list = command_list, output_dir = "./output", max_workers = 6)
