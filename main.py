9cd1a325c1d2fc4412c830456a8f0536fd496f70
test <test@test.com>
2024-05-11 09:50:59 +0800

    feature/save command list to local file

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import command
+from typing import List
 
 if __name__ == "__main__":
-    print(command.config)
+    # passed: bool = command.check_config(config = command.config)
+    passed: bool = True
+
+    if passed:
+        command_list: List[str] = command.construct_command(config = command.config)
+
+        with open("commands.txt", "w") as f:
+            for command in command_list:
+                f.write(command + "\n")
