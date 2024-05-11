3b1fee05084b7d785531b629071c2f1dd8a94490
test <test@test.com>
2024-05-11 11:28:52 +0800

    update execution.py

--- execution.py
+++ execution.py
@@ -1,9 +1,10 @@
+import os
 import subprocess
 import concurrent.futures
 from typing import List
 from command import logger
 
-def execute(command_list: List[str], output_dir: str, max_workers: int = 5) -> None:
+def execute(command_list: List[dict], output_dir: str, max_workers: int = 5) -> None:
     """Execute commands in parallel and save output to files
 
     Args:
@@ -13,17 +14,23 @@ def execute(command_list: List[str], output_dir: str, max_workers: int = 5) -> N
     """
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
         futures = []
-        for i, command in enumerate(command_list):
-            output_file = f"{output_dir}/output_{i}.txt"
+        for command in command_list:
+            output_file = f"{output_dir}/{command['tag']}.txt"
             futures.append(executor.submit(execute_command, command, output_file))
         for future in concurrent.futures.as_completed(futures):
             future.result()
 
 def execute_command(command: str, output_file: str) -> None:
+    output_dir = os.path.dirname(output_file)
+    if not os.path.exists(output_dir):
+        try:
+            os.makedirs(output_dir)
+        except FileExistsError:
+            pass    # concurrent execution
+
     try:
         with open(output_file, "w") as f:
             logger.info(f"Running: {command}")
-            subprocess.run(command, shell=True, stdout=f, stderr=subprocess.STDOUT, check=True)
-        logger.info(f"Output saved to {output_file}")
+            subprocess.run(command['command'], shell=True, stdout=f, stderr=subprocess.STDOUT, check=True)
     except subprocess.CalledProcessError as e:
         logger.warning(f"Error executing command '{command}': {e}")
